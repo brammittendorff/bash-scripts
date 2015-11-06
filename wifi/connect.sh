ed1e4598d95b812b45f138ea9100824d260d9595
root <root@localhost.localdomain>
2015-11-06 17:20:36 -0500

    added a wifi script for wpa_supplicant

---
+++ wifi/connect.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+IWLIST_FILE=".iwlist.cache"
+WPA_SUPPLICANT_FILE="/etc/wpa_supplicant/wpa_supplicant.conf"
+
+for i in "$@"
+do
+case $i in
+    -i=*|--interface=*)
+    INTERFACE=`echo $i | sed 's/[-a-zA-Z0-9]*=//'`
+    ;;
+    -s=*|--ssid=*)
+    SSID=`echo $i | sed 's/[-a-zA-Z0-9]*=//'`
+    ;;
+    -p=*|--password=*)
+    PASSWORD=`echo $i | sed 's/[-a-zA-Z0-9]*=//'`
+    ;;
+    *)
+
+    ;;
+esac
+done
+
+if [ ! -z $INTERFACE ]; then
+    if [ ! -f $IWLIST_FILE ]; then
+        echo "Scanning $INTERFACE and writing results to $IWLIST_FILE this may take a while..."
+        sudo iwlist $INTERFACE scan > $IWLIST_FILE
+    else
+        cat $IWLIST_FILE
+    fi
+elif [ ! -z $SSID ] || [ ! -z $PASSWORD ]; then
+    sudo sh -c "wpa_passphrase $SSID $PASSWORD >> $WPA_SUPPLICANT_FILE"
+    echo "Wifi $SSID with password **** written to file $WPA_SUPPLICANT_FILE"
+else
+    echo "Usage:  connect.sh [INTERFACE] [SSID] [PASSWORD]"
+    echo "  -i, --interface         Interface to scan for wifi"
+    echo "  -s, --ssid              The ssid of your wifi"
+    echo "  -p, --password          Wpa password"
+fi
